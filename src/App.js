96e951b4ec94fb0709cd363659a461da3a66b25e
Emídio Rebouças <emidioreb@gmail.com>
2021-10-22 22:20:20 -0300

    header parcialmente pronto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import M from 'materialize-css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Search from './components/Search';
@@ -7,18 +8,24 @@ import Sobre from './components/Sobre';
 import Artigos from './components/Artigos';
 import Contatos from './components/Contatos';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Search />
-      <Atendimentos />
-      <Sobre />
-      <Artigos />
-      <Contatos />
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    M.AutoInit();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Search />
+        <Atendimentos />
+        <Sobre />
+        <Artigos />
+        <Contatos />
+      </div>
+    );
+  }
 }
 
 export default App;
