8860988e0281729bb110dc79a52c3caa90ac057d
Emídio Rebouças <emidioreb@gmail.com>
2021-10-22 23:51:29 -0300

    inicio do main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../css/main.css';
+import emilcyFoto from '../imagens/emilcy.png';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        <h1>Main</h1>
+        <p id="main-text">Eu te ajudo a se tornar mãe com segurança e amor.</p>
+        {/* <div className="buttons">
+          <a className="waves-effect waves-light btn" href="# ">button</a>
+          <a className="waves-effect waves-light btn" href="# ">button</a>
+        </div> */}
+        <div className="apresentacao">
+          <img className="materialboxed" width="150" src={emilcyFoto} alt="foto-emilcy" />
+          <div id="name">Dra. Emilcy Rebouças</div>
+          <div id="obstetra">Genecologista Obstetra</div>
+        </div>
       </div>
     );
   }
